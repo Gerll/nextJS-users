d5faeca208eeabccf59a6822ab591b9e42bb65aa
gerll <gerll.bm@hotmail.com>
2023-09-28 23:49:18 -0700

    Created pages about and services

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente 
+                corporis numquam nihil cupiditate incidunt odio sequi, perspiciatis 
+                accusamus, neque dolores dicta voluptate. Delectus excepturi quos laboriosam 
+                animi autem saepe quae! Velit amet nemo corrupti natus iste aliquam aperiam 
+                consectetur modi asperiores dolorem repellat, voluptatibus ad obcaecati! 
+                Aliquid officiis, dolores sed porro voluptatum quam, quibusdam eligendi 
+                similique doloribus reprehenderit modi deserunt praesentium consequuntur 
+                beatae alias explicabo odio veniam quasi, sunt adipisci recusandae. 
+                Dolorum officia aliquam libero quam quibusdam necessitatibus 
+                cum nam illo autem quas, dicta accusamus temporibus voluptas. 
+                Voluptatibus, saepe dicta. Rem soluta eius odio placeat tempore 
+                consequuntur autem a quod.
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
