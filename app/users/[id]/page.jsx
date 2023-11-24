931817110c3e2a94048fb826179ae09e2fe205cc
gerll <gerll.bm@hotmail.com>
2023-11-24 15:20:59 -0800

    User details server-side rendering

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-function UserPage() {
+async function getUser(id) {
+    const res = await fetch(`https://reqres.in/api/users/${id}`)
+    const data = await res.json();
+    return data.data;
+}
+
+async function UserPage({params}) {
+    /* console.log(params) */
+
+    const user = await getUser(params.id)
+
     return (
         <div>
             <h1>User Details</h1>
+            <div>
+                <img src={user.avatar} alt={user.email} />
+                <h3>{user.id} {user.first_name} {user.last_name} </h3>
+                <p>{user.email}</p>
+            </div>
         </div>
     )
 }
